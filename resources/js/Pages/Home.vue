1ba2774d6fbc8c518cfb89588f3dda8e2797e3bf
Jean Sacren <sakiwit@gmail.com>
2024-09-03 19:05:28 -0600

    Add Home page of Vue
    Use sail artisan tool to create a new vue page for home page.
    
    $ sail artisan make:vue Home
    Page Home created successfully.
    
    Build the bare minimal Home page to include greeting variable and
    display its value.
    
    Update the routes of root URI to Home vue page.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+
+    {{ greeting }}
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                greeting: 'Hello World!',
+            };
+        },
+    };
+</script>
