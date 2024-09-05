104522f7f87b6fbb2076d85f59902281b94400aa
Jean Sacren <sakiwit@gmail.com>
2024-09-05 16:27:51 -0600

    Add 'Click me' button
    Add the button and use Tailwind utilities to style it.  This shows
    Tailwind is quite flexible.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -8,6 +8,10 @@
             <p class="flex justify-center mt-4">
             {{ greeting }} ({{ greeting.length }})
             </p>
+
+            <div class="flex justify-center mt-4">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click me</button>
+            </div>
         </div>
 </template>
 
