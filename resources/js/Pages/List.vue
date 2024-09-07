e9567cdf7c608afd1ac58c3ae8f43ea8ccb9c98e
Jean Sacren <sakiwit@gmail.com>
2024-09-07 14:49:56 -0600

    Add Vue page for a list of procedures
    Use sail artisan make:vue to initialize a new Vue page.
    
    $ sail artisan make:vue List
    
    Add a list of procedures to the page and style it with Tailwind CSS.

---
+++ resources/js/Pages/List.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen grid place-content-center">
+        <h2 class="mb-2 font-bold">Procedures</h2>
+        <ul>
+            <li>Patch kernel <input type="checkbox"></li>
+            <li>Update systemd <input type="checkbox"></li>
+            <li>Run docker <input type="checkbox"></li>
+            <li>Install Laravel <input type="checkbox"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
