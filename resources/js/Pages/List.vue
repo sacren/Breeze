545e8a2afe42f2d29374e02f4f2fd2e3d320f1aa
Jean Sacren <sakiwit@gmail.com>
2024-09-07 16:24:27 -0600

    Use Vue to dynamically construct list
    First build the list of procedures in Vue.  With the list built, now use
    it to dynamically populate the list in lieu of the static markup.

--- resources/js/Pages/List.vue
+++ resources/js/Pages/List.vue
@@ -2,15 +2,22 @@
     <div class="min-h-screen grid place-content-center">
         <h2 class="mb-2 font-bold">Procedures</h2>
         <ul>
-            <li>Patch kernel <input type="checkbox"></li>
-            <li>Update systemd <input type="checkbox"></li>
-            <li>Run docker <input type="checkbox"></li>
-            <li>Install Laravel <input type="checkbox"></li>
+            <li v-for="p in procedures">{{ p.name }} <input type="checkbox"></li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            procedures: [
+                { name: 'Patch kernel' },
+                { name: 'Update systemd' },
+                { name: 'Run docker' },
+                { name: 'Install Laravel' },
+            ],
+        };
+    },
 };
 </script>
