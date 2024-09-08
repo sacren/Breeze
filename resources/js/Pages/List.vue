23b813cd96902270f2a0eddd609883dcf88a4cd6
Jean Sacren <sakiwit@gmail.com>
2024-09-08 09:13:15 -0600

    Use more sensible variable names
    Use jobs and status for variable names in lieu of procedures and
    checked.

--- resources/js/Pages/List.vue
+++ resources/js/Pages/List.vue
@@ -2,20 +2,20 @@
     <div class="min-h-screen grid place-content-center">
         <h2 class="mb-2 font-bold">To Do</h2>
         <ul>
-            <li v-for="p in procedures">
+            <li v-for="job in jobs">
                 <label>
-                    {{ p.name }}
-                    <input type="checkbox" v-model="p.checked">
+                    {{ job.name }}
+                    <input type="checkbox" v-model="job.status">
                 </label>
             </li>
         </ul>
 
         <h2 class="mt-8 mb-2 font-bold">Completed</h2>
         <ul>
-            <li v-for="p in procedures">
+            <li v-for="job in jobs">
                 <label>
-                    {{ p.name }}
-                    <input type="checkbox" v-model="p.checked">
+                    {{ job.name }}
+                    <input type="checkbox" v-model="job.status">
                 </label>
             </li>
         </ul>
@@ -26,11 +26,11 @@
 export default {
     data() {
         return {
-            procedures: [
-                { name: 'Patch kernel', checked: false },
-                { name: 'Update systemd', checked: false },
-                { name: 'Run docker', checked: false },
-                { name: 'Install Laravel', checked: false },
+            jobs: [
+                { name: 'Patch kernel', status: false },
+                { name: 'Update systemd', status: false },
+                { name: 'Run docker', status: false },
+                { name: 'Install Laravel', status: false },
             ],
         };
     },
