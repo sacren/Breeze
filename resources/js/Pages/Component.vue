46b6bd08c2a519b335b8f90e74249987e4443f0d
Jean Sacren <sakiwit@gmail.com>
2024-09-09 14:32:22 -0600

    Add Vue page for components
    Use sail artisan make:vue to initialize a new Vue page.
    
    $ sail artisan make:vue Component
    
    Build app-button component from scratch and use alert() to show the
    working of app-button component.  Use Tailwind for styling.

---
+++ resources/js/Pages/Component.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="min-h-screen grid place-content-center">
+        <app-button></app-button>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        'app-button': {
+            mounted() {
+                alert('Hello Vue!');
+            },
+        },
+    },
+};
+</script>
