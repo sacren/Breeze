0e243be0ebf57d4aa372668cb47961cb89797d8b
Jean Sacren <sakiwit@gmail.com>
2024-09-11 19:10:32 -0600

    Use Javascript object for class
    The use of v-bind to class lets expression to assign a list of Tailwind
    utilities to it.  The expression is made of Javascript object with the
    key of the list and the value of true.  The true value applies the list
    to the class.  If the value is false, it doesn't apply the list.

--- resources/js/Components/BreezeButton.vue
+++ resources/js/Components/BreezeButton.vue
@@ -1,7 +1,8 @@
 <template>
     <button
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-gray-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" :disabled="processing"
-    >
+    :class="{
+             'px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-gray-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150': true,
+             }" :disabled="processing">
         <slot />
     </button>
 </template>
