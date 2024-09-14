9f5969b4e234eac6bba873d77f4c27d53d5f7fb0
Jean Sacren <sakiwit@gmail.com>
2024-09-13 22:31:47 -0600

    Add spinner for is-loading class
    Add a spinner to the component and use it for app-button.  Let the
    spinning effect show on the button when is-loading class is true.

--- resources/js/Components/BreezeButton.vue
+++ resources/js/Components/BreezeButton.vue
@@ -5,7 +5,7 @@
              'bg-green-800': type === 'primary',
              'bg-red-800': type === 'secondary',
              'bg-blue-800': type === 'muted',
-             'is-loading': processing,
+             'is-loading text-transparent': processing,
              }" :disabled="processing">
         <slot />
     </button>
@@ -26,3 +26,25 @@
         },
     };
 </script>
+
+<style scoped>
+@keyframes spinner {
+    to {transform: rotate(360deg);}
+}
+
+.is-loading:before {
+    content: '';
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 20px;
+    height: 20px;
+    margin-top: -10px;
+    margin-left: -10px;
+    border-radius: 50%;
+    border: 2px solid #ccc;
+    border-top-color: #000;
+    animation: spinner .6s linear infinite;
+}
+</style>
