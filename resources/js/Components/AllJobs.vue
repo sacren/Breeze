58719be74660c7b9c18b2492d6dfeda543f9a8c8
Jean Sacren <sakiwit@gmail.com>
2024-09-15 20:49:11 -0600

    Add Applist vue page and the component
    Use sail artisan make:vuecomponent to initialize AllJobs component for
    Applist vue page.  Build AllJobs component out of List vue page.
    
    Use sail artisan make:vue to build Applist vue page and import AllJobs
    component.  After AllJobs component is registered in Applist, Applist
    serves as if it were List vue page.

---
+++ resources/js/Components/AllJobs.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="min-h-screen grid place-content-center">
+        <h2 v-show="todo.length" class="mb-2 font-bold">To Do</h2>
+        <ul>
+            <li
+                v-for="job in todo"
+                :key="job.id">
+
+                <label>
+                    {{ job.name }}
+                    <input
+                            name="todo"
+                            type="checkbox"
+                            v-model="job.status">
+                </label>
+            </li>
+        </ul>
+
+        <h2 v-show="completed.length" class="mt-8 mb-2 font-bold">Completed</h2>
+        <ul>
+            <li
+                v-for="job in completed"
+                :key="job.id">
+
+                <label>
+                    {{ job.name }}
+                    <input
+                            name="completed"
+                            type="checkbox"
+                            v-model="job.status">
+                </label>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                { name: 'Patch kernel', status: false, id: 1 },
+                { name: 'Update systemd', status: false, id: 2 },
+                { name: 'Run docker', status: false, id: 3 },
+                { name: 'Install Laravel', status: false, id: 4 },
+            ],
+        };
+    },
+
+    computed: {
+        todo() {
+            return this.jobs.filter(a => !a.status);
+        },
+
+        completed() {
+            return this.jobs.filter(a => a.status);
+        },
+    },
+};
+</script>
---
+++ resources/js/Pages/Applist.vue
@@ -0,0 +1,18 @@
+<template>
+    <Head title="Applist" />
+    <div>
+        <AllJobs />
+    </div>
+</template>
+
+<script>
+    import { Head } from '@inertiajs/vue3';
+    import AllJobs from '@/Components/AllJobs.vue';
+
+    export default {
+        components: {
+            Head,
+            AllJobs,
+        },
+    };
+</script>
