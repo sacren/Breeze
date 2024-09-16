c63024fe4ce3f13516cbbcc9f9bd984042039169
Jean Sacren <sakiwit@gmail.com>
2024-09-15 23:50:07 -0600

    Add jobList component for to-do or completed jobs
    Loop through to-do or completed job list is almost identical.  Use one
    component to replace both.  In each list, use props for dynamic values
    to build a generic component for either to-do or completed jobs.
    
    Use allJobs in lieu of jobs as the key for the full list of jobs,
    regardless of the status.

--- resources/js/Components/AllJobs.vue
+++ resources/js/Components/AllJobs.vue
@@ -1,60 +1,48 @@
 <template>
     <div class="min-h-screen grid place-content-center">
-        <h2 v-show="todo.length" class="mb-2 font-bold">To Do</h2>
-        <ul>
-            <li
-                v-for="job in todo"
-                :key="job.id">
+        <JobList
+            :jobList="todo"
+            listName="To Do"
+            fieldName="todo"
+        />
 
-                <label>
-                    {{ job.name }}
-                    <input
-                            name="todo"
-                            type="checkbox"
-                            v-model="job.status">
-                </label>
-            </li>
-        </ul>
+        <div class="mt-8"></div>
 
-        <h2 v-show="completed.length" class="mt-8 mb-2 font-bold">Completed</h2>
-        <ul>
-            <li
-                v-for="job in completed"
-                :key="job.id">
-
-                <label>
-                    {{ job.name }}
-                    <input
-                            name="completed"
-                            type="checkbox"
-                            v-model="job.status">
-                </label>
-            </li>
-        </ul>
+        <JobList
+            :jobList="completed"
+            listName="Completed"
+            fieldName="completed"
+        />
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            jobs: [
-                { name: 'Patch kernel', status: false, id: 1 },
-                { name: 'Update systemd', status: false, id: 2 },
-                { name: 'Run docker', status: false, id: 3 },
-                { name: 'Install Laravel', status: false, id: 4 },
-            ],
-        };
-    },
+    import JobList from './JobList.vue';
 
-    computed: {
-        todo() {
-            return this.jobs.filter(a => !a.status);
+    export default {
+        components: {
+            JobList,
         },
 
-        completed() {
-            return this.jobs.filter(a => a.status);
+        data() {
+            return {
+                allJobs: [
+                    { name: 'Patch kernel', status: false, id: 1 },
+                    { name: 'Update systemd', status: false, id: 2 },
+                    { name: 'Run docker', status: false, id: 3 },
+                    { name: 'Install Laravel', status: false, id: 4 },
+                ],
+            };
         },
-    },
-};
+
+        computed: {
+            todo() {
+                return this.allJobs.filter(a => !a.status);
+            },
+
+            completed() {
+                return this.allJobs.filter(a => a.status);
+            },
+        },
+    };
 </script>
---
+++ resources/js/Components/JobList.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2 v-show="jobList.length" class="mb-2 font-bold">{{ listName }}</h2>
+    <ul>
+        <li
+                v-for="job in jobList"
+                :key="job.id">
+
+            <label>
+                {{ job.name }}
+                <input
+                        name="fieldName"
+                        type="checkbox"
+                        v-model="job.status">
+            </label>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: {
+            jobList: Array,
+            listName: String,
+            fieldName: String,
+        },
+    };
+</script>
