506830d06bc664c0721f606876d7ea8ac49a01fb
Jean Sacren <sakiwit@gmail.com>
2024-09-16 21:24:37 -0600

    Add JobItem component
    In JobList, the list of jobs is an integral part by itself.  Build this
    part as a component that forms JobItem.  With JobItem built, replace it
    with the pertinent part of JobList to make JobList simpler.

---
+++ resources/js/Components/JobItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>
+        <label>
+            {{ job.name }}
+            <input
+                    name="fieldName"
+                    type="checkbox"
+                    v-model="job.status">
+        </label>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: {
+            job: Object,
+            fieldName: String,
+        },
+    };
+</script>
--- resources/js/Components/JobList.vue
+++ resources/js/Components/JobList.vue
@@ -1,23 +1,23 @@
 <template>
     <h2 v-show="jobList.length" class="mb-2 font-bold">{{ listName }}</h2>
     <ul>
-        <li
-                v-for="job in jobList"
-                :key="job.id">
-
-            <label>
-                {{ job.name }}
-                <input
-                        name="fieldName"
-                        type="checkbox"
-                        v-model="job.status">
-            </label>
-        </li>
+        <JobItem
+            v-for="job in jobList"
+            :key="job.id"
+            :job="job"
+            :fieldName="fieldName"
+        />
     </ul>
 </template>
 
 <script>
+    import JobItem from './JobItem.vue';
+
     export default {
+        components: {
+            JobItem,
+        },
+
         props: {
             jobList: Array,
             listName: String,
