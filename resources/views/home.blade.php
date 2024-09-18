290030867a058691a9870cb76a27938a89801851
Jean Sacren <sakiwit@gmail.com>
2024-09-17 22:50:15 -0600

    Add more examples of using echo and print
    PHP is very similar to bash as to echo and print.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,3 +2,23 @@
 <?php
 
 echo 'Hello World!';
+print 'Hello World!';
+
+/* print is the same as echo, but the statement returns 1.
+ * print can be used with expressions while echo cannot.
+ *
+ * This is wrong:
+ * print echo 'Hello World!';
+ */
+echo print 'Hello World!';
+
+# Parentheses are optional.
+print('Hello World!');
+echo('Hello World!');
+
+// Concatenate with echo, but not with print.
+echo 'Hello', ' ', 'World!';
+
+// Escape single quotes or use double quotes.
+echo 'What\'s up?';
+echo "What's up?";
