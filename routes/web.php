cdcdac5600f050288f4ed76cc457863c2a7c9b8d
Jean Sacren <sakiwit@gmail.com>
2024-09-17 20:45:01 -0600

    Add Home page for PHP
    Use views and its component to set up for PHP.

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/php', function () {
+    return view('home');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
