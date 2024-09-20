6413239951b06f86132935ebc78ae527e3b131a1
Jean Sacren <sakiwit@gmail.com>
2024-09-20 13:18:04 -0600

    Merge Applist into List
    Applist was used as the transitional stage for List when components of
    AllJobs and JobList were built for it.  Now all the necessary components
    are built and the purpose of Applist is fulfilled.  Merge Applist into
    List.

--- resources/js/Pages/List.vue
+++ resources/js/Pages/List.vue
@@ -1,50 +1,16 @@
 <template>
-    <div class="grid place-content-center min-h-screen space-y-8">
-        <section v-show="todo.length">
-            <JobList
-                    :jobList="todo"
-                    listName="To Do"
-                    fieldName="todo">
-            </JobList>
-        </section>
-
-        <section v-show="completed.length">
-            <JobList
-                    :jobList="completed"
-                    listName="Completed"
-                    fieldName="completed">
-            </JobList>
-        </section>
-    </div>
+    <Head title="List"></Head>
+    <AllOver></AllOver>
 </template>
 
 <script>
-    import JobList from '@/Components/JobList.vue';
+    import { Head } from '@inertiajs/vue3';
+    import AllOver from '@/Components/AllJobs.vue';
 
     export default {
         components: {
-            JobList,
-        },
-
-        data() {
-            return {
-                jobs: [
-                    { name: 'Patch kernel', status: false, id: 1 },
-                    { name: 'Update systemd', status: false, id: 2 },
-                    { name: 'Run docker', status: false, id: 3 },
-                    { name: 'Install Laravel', status: false, id: 4 },
-                ],
-            };
-        },
-
-        computed: {
-            todo() {
-                return this.jobs.filter(a => !a.status);
-            },
-
-            completed() {
-                return this.jobs.filter(a => a.status);
-            },
+            Head,
+            AllOver,
         },
     };
 </script>
