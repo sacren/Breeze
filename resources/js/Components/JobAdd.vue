38843bff89ce7eb7d1cec8ed0e34dd6445bae5f1
Jean Sacren <sakiwit@gmail.com>
2024-09-26 15:49:44 -0600

    Add JobAdd component for the form
    Use sail artisan tool to add JobAdd component and build it with the form
    part of AllJobs component.
    
    Temporarily use alert for addJob method and add newJob data.

---
+++ resources/js/Components/JobAdd.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit.prevent="addJob">
+        <div class="border-2 border-green-500">
+            <input
+                    v-model="newJob"
+                    placeholder="New Job"
+                    type="text"
+                    name="newJob"
+                    class="p-2">
+            <button type="submit" class="p-2 border-l border-red-500">Add</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                newJob: null,
+            };
+        },
+
+        methods: {
+            addJob() {
+                alert('Add a new job');
+            },
+        },
+    };
+</script>
