0d8eb5954821ade663fab30a7be0a4b368342b81
Jean Sacren <sakiwit@gmail.com>
2024-09-27 17:20:58 -0600

    Add the size of job list
    This shows how easy and powerful it is for Vue to apply props across
    components, even in a parent-child relation.
    
    Use span for inline rather than block as to text.

--- resources/js/Components/JobList.vue
+++ resources/js/Components/JobList.vue
@@ -1,5 +1,8 @@
 <template>
-    <h2 v-show="jobList.length" class="mb-2 font-bold">{{ listName }}</h2>
+    <h2 v-show="jobList.length" class="mb-2 font-bold">
+        {{ listName }}
+        <span class="ml-2 text-red-500">({{ jobList.length }})</span>
+    </h2>
     <ul class="border-2 border-green-500 divide-y divide-green-500">
         <job-item
             v-for="job in jobList"
