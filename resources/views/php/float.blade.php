3db6d22bc422719b1fefbdf7333f70d4bef40cd6
Jean Sacren <sakiwit@gmail.com>
2024-09-28 16:03:24 -0600

    Add special examples of float type

--- resources/views/php/float.blade.php
+++ resources/views/php/float.blade.php
@@ -1,24 +1,34 @@
 <x-layout></x-layout>
 <?php
 
-$number = 13; // 13, int type
-var_dump($number);
+$number = 13;
+var_dump($number); // int(13)
+echo '<br>';
+var_dump((float) $number); // float(13), float type yet not a decimal number
 echo '<br>';
 
-$number = 13.5; // 13.5, float type
-var_dump($number);
+$number = 'abc';
+var_dump((float) $number); // float(0)
 echo '<br>';
 
-$number = 13_000.5; // 13000.5, better readability
-var_dump($number);
+$number = '13.5abc';
+var_dump((float) $number); // float(13.5)
 echo '<br>';
 
-$number = 13.5e3; // 13500
-var_dump($number); // float type without decimal point
+$number = 13.5;
+var_dump($number); // float(13.5)
 echo '<br>';
 
-$number = 13.5e-3; // 0.0135
-var_dump($number);
+$number = 13_000.5;
+var_dump($number); // float(13000.5)
+echo '<br>';
+
+$number = 13.5e3;
+var_dump($number); // float(13500), float type yet not a decimal number
+echo '<br>';
+
+$number = 13.5e-3;
+var_dump($number); // float(0.0135)
 echo '<br>';
 
 // predefined float constants
@@ -27,3 +37,36 @@ echo $number . '<br>';
 
 $number = PHP_FLOAT_MIN;
 echo $number . '<br>';
+
+// float is approximate, never compare
+$number = (0.1 + 0.7) * 10; // 7.9999999999999999
+echo $number . '<br>'; // 8
+echo floor($number) . '<br>'; // 7
+
+$number = (0.1 + 0.2) * 10; // 3.0000000000000004
+echo $number . '<br>'; // 3
+echo ceil($number) . '<br>'; // 4
+
+// NAN
+$number = NAN;
+echo $number . '<br>'; // NAN
+$number = log(-1);
+echo $number . '<br>'; // NAN
+var_dump(is_nan($number)); // bool(true)
+echo '<br>';
+var_dump(is_infinite($number)); // bool(false)
+echo '<br>';
+var_dump(is_finite($number)); // bool(false)
+echo '<br>';
+
+// INF
+$number = INF;
+echo $number . '<br>'; // INF
+$number = PHP_FLOAT_MAX + PHP_FLOAT_MAX;
+echo $number . '<br>'; // INF
+var_dump(is_infinite($number)); // bool(true)
+echo '<br>';
+var_dump(is_finite($number)); // bool(false)
+echo '<br>';
+var_dump(is_nan($number)); // bool(false)
+echo '<br>';
