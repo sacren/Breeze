5dda37bf53076b8f9a7b61a0116264081d191fef
Jean Sacren <sakiwit@gmail.com>
2024-10-05 23:39:22 -0600

    Add JobTags component
    Build JobTags component for custom job-tags element in jobList.

---
+++ resources/js/Components/JobTags.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex gap-2">
+        <button
+            v-for="tag in tags"
+            @click="currentTag = tag"
+            class="border-2 rounded-md border-blue-500 px-2 text-xs"
+            :class="{ 'bg-blue-500 text-white': currentTag === tag }"
+            :key="tag">
+            {{ tag }}
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            tagList: Array,
+        },
+
+        data() {
+            return {
+                currentTag: 'all',
+            };
+        },
+
+        computed: {
+            tags() {
+                const tags = this.tagList.map(a => a.tag);
+
+                return [ 'all', ...new Set(tags) ];
+            },
+        },
+    };
+</script>
