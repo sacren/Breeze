859b124074428a96dd6b7fdb488166a650b31bee
Jean Sacren <sakiwit@gmail.com>
2024-10-07 14:56:36 -0600

    Add examples of other operators

--- resources/views/php/operator.blade.php
+++ resources/views/php/operator.blade.php
@@ -65,3 +65,95 @@ $tmp1 = -10;
 $tmp2 = 3;
 var_dump($tmp1 % $tmp2); // int(-1)
 echo "<br>";
+
+// assignment operators
+$tmp1 = $tmp2 = 10;
+var_dump($tmp1, $tmp2); // int(10) int(10)
+echo "<br>";
+
+$tmp1 += $tmp2;
+echo $tmp1 . "<br>"; // 20
+$tmp1 -= $tmp2;
+echo $tmp1 . "<br>"; // 10
+$tmp1 *= $tmp2;
+echo $tmp1 . "<br>"; // 100
+$tmp1 /= $tmp2;
+echo $tmp1 . "<br>"; // 10
+
+// string concatenation
+$tmp1 = 'Hello';
+$tmp1 .= ' world';
+echo $tmp1 . "<br>";
+
+// comparison operators
+$tmp1 = 5;
+$tmp2 = 3;
+var_dump($tmp1 == $tmp2); // bool(false), type conversion
+echo "<br>";
+var_dump($tmp1 === $tmp2); // bool(false), compare type and value
+echo "<br>";
+
+$tmp2 = '5';
+var_dump($tmp1 == $tmp2); // bool(true)
+echo "<br>";
+var_dump($tmp1 === $tmp2); // bool(false)
+echo "<br>";
+var_dump($tmp1 != $tmp2); // bool(false)
+echo "<br>";
+var_dump($tmp1 !== $tmp2); // bool(true)
+echo "<br>";
+var_dump($tmp1 <> $tmp2); // bool(false)
+echo "<br>";
+
+$tmp2 = 10;
+var_dump($tmp1 < $tmp2); // bool(true)
+echo "<br>";
+var_dump($tmp1 > $tmp2); // bool(false)
+echo "<br>";
+var_dump($tmp1 <= $tmp2); // bool(true)
+echo "<br>";
+var_dump($tmp1 >= $tmp2); // bool(false)
+echo "<br>";
+
+// spaceship operator
+$tmp1 = 10;
+$tmp2 = 5;
+var_dump($tmp1 <=> $tmp2); // int(1)
+echo "<br>";
+$tmp2 = 10;
+var_dump($tmp1 <=> $tmp2); // int(0)
+echo "<br>";
+$tmp2 = 20;
+var_dump($tmp1 <=> $tmp2); // int(-1)
+echo "<br>";
+$tmp2 = '10';
+var_dump($tmp1 <=> $tmp2); // int(0)
+echo "<br>";
+
+// use strict type for comparison
+$tmp1 = 'Hello';
+$tmp2 = strpos($tmp1, 'H');
+
+if ($tmp2 === false) { // wrong: $tmp2 == false
+    echo 'Not found';
+} else {
+    echo 'Found at position ' . $tmp2;
+}
+echo "<br>";
+
+// ternary operator
+$result = $tmp2 === false ? 'Not found' : 'Found at position ' . $tmp2;
+echo $result . "<br>";
+
+// null coalescing operator
+$tmp1 = null;
+$tmp2 = $tmp1 ?? 'default';
+var_dump($tmp2); // string(7) "default"
+echo "<br>";
+$tmp1 = false;
+$tmp2 = $tmp1 ?? 'default';
+var_dump($tmp2); // bool(false)
+echo "<br>";
+$tmp2 = $tmp3 ?? 'default';
+var_dump($tmp2); // string(7) "default"
+echo "<br>";
