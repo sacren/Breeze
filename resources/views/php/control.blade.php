f751d003374036c26ebc070bd561d55fd8e499a1
Jean Sacren <sakiwit@gmail.com>
2024-10-10 15:28:21 -0600

    Add nested if for plus (+)

--- resources/views/php/control.blade.php
+++ resources/views/php/control.blade.php
@@ -7,12 +7,28 @@ $score = 75;
 
 if ($score >= 90) {
     echo 'A';
+
+    if ($score >= 95) {
+        echo '+';
+    }
 } elseif ($score >= 80) { // use elseif instead of else if
     echo 'B';
+
+    if ($score >= 85) {
+        echo '+';
+    }
 } elseif ($score >= 70) {
     echo 'C';
+
+    if ($score >= 75) {
+        echo '+';
+    }
 } elseif ($score >= 60) {
     echo 'D';
+
+    if ($score >= 65) {
+        echo '+';
+    }
 } else {
     echo 'F';
 }
