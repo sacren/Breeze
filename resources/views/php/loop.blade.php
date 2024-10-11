5a5bbdfee4900e08f493dfa607b11d42dac3fd1a
Jean Sacren <sakiwit@gmail.com>
2024-10-11 15:03:53 -0600

    Add an example of break 2

--- resources/views/php/loop.blade.php
+++ resources/views/php/loop.blade.php
@@ -21,6 +21,18 @@ while (true) {
 
 echo '<br>';
 
+// use break 2
+$i = 0;
+while (true) {
+    while ($i === 10) {
+        break 2;
+    }
+
+    echo $i++;
+}
+
+echo '<br>';
+
 // use continue for odd numbers
 $i = 0;
 while ($i < 10) {
