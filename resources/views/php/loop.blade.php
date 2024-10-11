b8b1ba7b1bb433c77ca43d6aca723f37c712d2fa
Jean Sacren <sakiwit@gmail.com>
2024-10-11 14:03:36 -0600

    Move examples of loop to its own file

---
+++ resources/views/php/loop.blade.php
@@ -0,0 +1,46 @@
+<x-layout></x-layout>
+<?php
+
+// while loop
+$i = 0;
+while ($i < 10) {
+    echo $i++; // without increment, will loop infinitely
+}
+
+echo '<br>';
+
+// use break
+$i = 0;
+while (true) {
+    if ($i >= 10) {
+        break;
+    }
+
+    echo $i++;
+}
+
+echo '<br>';
+
+// use continue for odd numbers
+$i = 0;
+while ($i < 10) {
+    if ($i % 2 === 0) {
+        $i++;
+        continue;
+    }
+
+    echo $i++;
+}
+
+echo '<br>';
+
+// : and endwhile
+$i = 0;
+while ($i < 10):
+    if ($i % 2 === 0) {
+        $i++;
+        continue;
+    }
+
+    echo $i++;
+endwhile;
