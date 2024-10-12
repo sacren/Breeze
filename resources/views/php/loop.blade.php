a5223d337a1d1d7543e673c1cda3c61d8b004be1
Jean Sacren <sakiwit@gmail.com>
2024-10-12 16:14:27 -0600

    Add examples of for and foreach loops

--- resources/views/php/loop.blade.php
+++ resources/views/php/loop.blade.php
@@ -64,3 +64,47 @@ $i = 0;
 do {
     echo $i++; // executed at least once
 } while ($i < 10);
+
+echo '<br>';
+
+// for loop
+for ($i = 0; $i < 10; $i++) {
+    echo $i;
+}
+
+echo '<br>';
+
+for ($i = 0; $i < 10; print $i, $i++) { // print in third part
+}
+
+echo '<br>';
+
+for ($i = 0; print $i, $i < 10; $i++) { // additional 10
+}
+
+echo '<br>';
+
+$fruits = ['apple', 'banana', 'orange'];
+
+// avoid unnecessary calls
+for ($i = 0, $total = count($fruits); $i < $total; $i++) {
+    echo $fruits[$i] . '<br>';
+}
+
+// use foreach for arrays
+foreach ($fruits as $name) {
+    echo $name . '<br>';
+}
+
+// foreach with key
+foreach ($fruits as $key => $name) {
+    echo $key . ': ' . $name . '<br>';
+}
+
+// pass by reference
+foreach ($fruits as $key => &$name) { // & operator
+    $name = 'watermelon'; // modifies $fruits array
+}
+
+unset($name); // this is necessary to remove reference
+print_r($fruits);
