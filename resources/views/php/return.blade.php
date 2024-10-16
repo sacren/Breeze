c742f09ccb03fcdcf23b4a32812d9e1d2dbf7c5d
Jean Sacren <sakiwit@gmail.com>
2024-10-16 16:14:41 -0600

    Add examples of tick

--- resources/views/php/return.blade.php
+++ resources/views/php/return.blade.php
@@ -12,3 +12,15 @@ function sum(int $a, int $b) {
 // return; this ends the script
 
 echo sum(5, 10) . '<br>'; // 15
+
+function onTick() {
+    echo 'Tick<br>';
+}
+
+register_tick_function('onTick');
+
+declare(ticks = 3); // global scope
+
+for ($i = 0; $i < 6; $i++) {
+    echo $i . '<br>';
+}
